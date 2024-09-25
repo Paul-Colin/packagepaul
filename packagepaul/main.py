58757b0c0555c1d1c32f6e8d9438ee1806c0bb24
Paul-Colin <paul.colin@ig2i.centralelille.fr>
2024-09-25 15:08:14 +0200

    workflow

--- packagepaul/main.py
+++ packagepaul/main.py
@@ -1,6 +1,6 @@
-def packagepaul( a:int, b:int) -> int: 
+def packagepaul( a:int, b:int) -> int:
     if(a > 10):
-        return a + b + 2 
+        return a + b + 2
     return a + b
 
 test = packagepaul(8,10)
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,10 +1,15 @@
 import unittest
+from packagepaul.main import packagepaul , test
+#test
 
-from packagepaul.main import packagepaul, test 
-
-class TestMain(unittest.TestCase):
+class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(18,test)
+        test = packagepaul(8,10)
+        self.assertEqual(18, test)
 
-if __name__ == '__main__': 
-    unittest.TestCase
+    def test_something1(self):
+        test = packagepaul(12,10)
+        self.assertEqual(24, test)
+
+if __name__ == '__main__':
+    unittest.main()
